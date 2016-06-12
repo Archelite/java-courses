35de0923c003078af797c879275ad42f580cafff
Archelite <archelite011@gmail.com>
2016-06-12 14:04:02 +0400

    Update and rename LICENSE.md to Calculate.java

---
+++ Calculate.java
@@ -0,0 +1,9 @@
+public class Calculate {
+   public static void main (String[] args){
+   System.out.println("Hello World !");
+      int x = Integer.ParseInt(args[0]);
+      int z = Integer.ParseInt(args[1]);
+      int Sum = z+x;
+    System.out.println("Sum: "+Sum);
+   }
+}
